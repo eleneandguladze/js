863ccd5d6b0d1d85011cf763aaf00ca38c6c93ad
elene <elene.andguladze33b@geolab.edu.ge>
2024-06-28 00:38:24 +0400

    assignment-2

---
+++ assignment-2-eleneandguladze-main/README.md
@@ -0,0 +1,18 @@
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/ZNd9YJdH)
+# Homework2
+Write down what the following statements will return. Try to figure this out before putting the commands terminal.
+
+1. 2 == "2;
+2. 2 === 2;
+3. 10 % 3 === 0;
+4. 10 % 3 === 1;
+5. true && false;
+6. false || true;
+7. true || false;
+8. 5 + 8 >= 12 || 19 < 90 || !false;
+9. "Josh" == "josh" && !true && false && 17.9 + 0.1 == 18.1;
+10. 0.1 + 0.2 == 0.3;
+11. Math.min(0, 1, 2, 10, 11, 100) && Math.max(-100, -90, -1, -0.9, 0);
+12. Math.max(0, 1, 2, 10, 11, 100) && Math.min(-100, -90, -1, -0.9, 0);
+13. Math.min(0, 1, 2, 10, 11, 100) || Math.min(-100, -90, -1, -0.9, 0);
+14. !Math.min(0, 1, 2, 10, 11, 100) || !Math.max(-100, -90, -1, -0.9, 0);
---
+++ assignment-2-eleneandguladze-main/main.js
@@ -0,0 +1,43 @@
+// Write your code here
+//2 == "2";
+console.log(2 == "2");
+
+//2 === 2;
+console.log(2 === 2);
+
+//10 % 3 === 0;
+console.log(10 % 3 === 0);
+
+//10 % 3 === 1;
+console.log(10 % 3 === 1);
+
+//true && false;
+console.log(true && false);
+
+//false || true;
+console.log(false || true);
+
+//true || false;
+console.log(true || false);
+
+//5 + 8 >= 12 || 19 < 90 || !false;
+console.log(5 + 8 >= 12 || 19 < 90 || !false);
+
+//"Josh" == "josh" && !true && false && 17.9 + 0.1 == 18.1;
+console.log("Josh" == "josh" && !true && false && 17.9 + 0.1 == 18.1);
+
+//0.1 + 0.2 == 0.3;
+console.log(0.1 + 0.2 == 0.3);
+
+//Math.min(0, 1, 2, 10, 11, 100) && Math.max(-100, -90, -1, -0.9, 0);
+console.log(Math.min(0, 1, 2, 10, 11, 100) && Math.max(-100, -90, -1, -0.9, 0));
+
+//Math.max(0, 1, 2, 10, 11, 100) && Math.min(-100, -90, -1, -0.9, 0);
+console.log(Math.max(0, 1, 2, 10, 11, 100) && Math.min(-100, -90, -1, -0.9, 0));
+
+//Math.min(0, 1, 2, 10, 11, 100) || Math.min(-100, -90, -1, -0.9, 0);
+console.log(Math.min(0, 1, 2, 10, 11, 100) || Math.min(-100, -90, -1, -0.9, 0));
+
+//!Math.min(0, 1, 2, 10, 11, 100) || !Math.max(-100, -90, -1, -0.9, 0);
+console.log(!Math.min(0, 1, 2, 10, 11, 100) || !Math.max(-100, -90, -1, -0.9, 0));
+
